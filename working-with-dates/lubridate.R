04be0031860535fac7e3cf1559dba6bf1301f7ea
Rod Morgado <rodrigo@zooft.company>
2021-11-27 15:00:10 -0800

    calculating differences between different datetimes in multiple formats

--- working-with-dates/lubridate.R
+++ working-with-dates/lubridate.R
@@ -181,3 +181,27 @@ akl_hourly %>%
   count(day_hour) %>%
   filter(n != 2) %>% 
   arrange(desc(n))
+
+# The date of landing and moment of step
+date_landing <- mdy("July 20, 1969")
+moment_step <- mdy_hms("July 20, 1969, 02:56:15", tz = "UTC")
+
+# How many days since the first man on the moon?
+difftime(today(), date_landing, units = "days")
+
+# How many seconds since the first man on the moon?
+difftime(now(), moment_step, units = "secs")
+
+# Three dates
+mar_11 <- ymd_hms("2017-03-11 12:00:00", 
+  tz = "America/Los_Angeles")
+mar_12 <- ymd_hms("2017-03-12 12:00:00", 
+  tz = "America/Los_Angeles")
+mar_13 <- ymd_hms("2017-03-13 12:00:00", 
+  tz = "America/Los_Angeles")
+
+# Difference between mar_13 and mar_12 in seconds
+difftime(mar_13, mar_12, units = "secs")
+
+# Difference between mar_12 and mar_11 in seconds
+difftime(mar_12, mar_11, units = "secs")

ad5028f3ec83164f1b42900a1032db7cc08c5dd7
Rod Morgado <rodrigo@zooft.company>
2021-11-28 20:53:12 -0800

    working with time values without dates

--- working-with-dates/timezones.R
+++ working-with-dates/timezones.R
@@ -38,4 +38,16 @@ head(akl_hourly$datetime)
   
 # Are datetime and date_utc the same moments
 table(akl_hourly$datetime - akl_hourly$date_utc)
-  
+
+# Import auckland hourly data 
+akl_hourly <- read_csv("akl_weather_hourly_2016.csv")
+
+# Examine structure of time column
+str(akl_hourly$time)
+
+# Examine head of time column
+head(akl_hourly$time)
+
+# A plot using just time
+ggplot(akl_hourly, aes(x = time, y = temperature)) +
+  geom_line(aes(group = make_date(year, month, mday)), alpha = 0.2)

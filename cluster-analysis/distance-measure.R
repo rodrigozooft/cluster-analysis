89794929a3df074ee7380de49d4bdf1c28c55a02
Rod Morgado <rodrigo@zooft.company>
2022-01-06 14:08:19 -0800

    working with distance for cluster analysis

---
+++ cluster-analysis/distance-measure.R
@@ -0,0 +1,13 @@
+# Plot the positions of the players
+ggplot(two_players, aes(x = x, y = y)) + 
+  geom_point() +
+  # Assuming a 40x60 field
+  lims(x = c(-30,30), y = c(-20, 20))
+
+# Split the players data frame into two observations
+player1 <- two_players[1, ]
+player2 <- two_players[2, ]
+
+# Calculate and print their distance using the Euclidean Distance formula
+player_distance <- sqrt( (player1$y - player2$y)^2 + (player1$x - player2$x)^2 )
+player_distance

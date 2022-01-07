772de7520c07ebecd8ae600257405513678b6e6f
Rod Morgado <rodrigo@zooft.company>
2022-01-06 16:53:00 -0800

    using dendograms to compare the different linkages

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -81,3 +81,18 @@ count(lineup_k2_complete, cluster)
 # Plot the positions of the players and color them using their cluster
 ggplot(lineup_k2_complete, aes(x = x, y = y, color = factor(cluster))) +
   geom_point()
+
+  # Prepare the Distance Matrix
+dist_players <- dist(lineup)
+
+# Generate hclust for complete, single & average linkage methods
+hc_complete <- hclust(dist_players, method = "complete")
+hc_single <- hclust(dist_players, method = "single")
+hc_average <- hclust(dist_players, method = "average")
+
+# Plot & Label the 3 Dendrograms Side-by-Side
+# Hint: To see these Side-by-Side run the 4 lines together as one command
+par(mfrow = c(1,3))
+plot(hc_complete, main = 'Complete Linkage')
+plot(hc_single, main = 'Single Linkage')
+plot(hc_average, main = 'Average Linkage')

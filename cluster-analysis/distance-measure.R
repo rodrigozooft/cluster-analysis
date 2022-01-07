b99d84f06115489a2def80c900551869529cb817
Rod Morgado <rodrigo@zooft.company>
2022-01-06 16:29:00 -0800

    calculating the distances between one group (1-2) and other element 3

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -45,3 +45,20 @@ job_survey
 
 # Print the Distance Matrix
 dist_survey
+
+# Extract the pair distances
+distance_1_2 <- dist_players[1]
+distance_1_3 <- dist_players[2]
+distance_2_3 <- dist_players[3]
+
+# Calculate the complete distance between group 1-2 and 3
+complete <- max(c(distance_1_3, distance_2_3))
+complete
+
+# Calculate the single distance between group 1-2 and 3
+single <- min(c(distance_1_3, distance_2_3))
+single
+
+# Calculate the average distance between group 1-2 and 3
+average <- mean(c(distance_1_3, distance_2_3))
+average

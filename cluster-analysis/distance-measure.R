52a8f1da8eb0e654daedfac0793f3b1288917a3c
Rod Morgado <rodrigo@zooft.company>
2022-01-07 14:25:44 -0800

    using kmean to perform cluster analysis to the lineup dataframe

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -176,3 +176,16 @@ plot(dend_colored)
 segment_customers %>% 
   group_by(cluster) %>% 
   summarise_all(list(mean))
+
+# Build a kmeans model
+model_km2 <- kmeans(lineup, centers = 2)
+
+# Extract the cluster assignment vector from the kmeans model
+clust_km2 <- model_km2$cluster
+
+# Create a new data frame appending the cluster assignment
+lineup_km2 <- mutate(lineup, cluster = clust_km2)
+
+# Plot the positions of the players and color them using their cluster
+ggplot(lineup_km2, aes(x = x, y = y, color = factor(cluster))) +
+  geom_point()

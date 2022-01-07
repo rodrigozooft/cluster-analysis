79fc32297ad2a1bdeea830775c5a29f103f302f1
Rod Morgado <rodrigo@zooft.company>
2022-01-07 15:28:07 -0800

    using hierachical cluster to classify at the height of 100,000 using average method and euclidean distance

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -284,3 +284,21 @@ count(segment_customers, cluster)
 segment_customers %>% 
   group_by(cluster) %>% 
   summarise_all(list(mean))
+
+# Calculate Euclidean distance between the occupations
+dist_oes <- dist(oes, method = "euclidean")
+
+# Generate an average linkage analysis 
+hc_oes <- hclust(dist_oes, method = "average")
+
+# Create a dendrogram object from the hclust variable
+dend_oes <- as.dendrogram(hc_oes)
+
+# Plot the dendrogram
+plot(dend_oes)
+
+# Color branches by cluster formed from the cut at a height of 100000
+dend_colored <- color_branches(dend_oes, h = 100000)
+
+# Plot the colored dendrogram
+plot(dend_colored)

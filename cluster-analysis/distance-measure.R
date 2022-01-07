0fa357a89531d727447b0b188aef5aae5d8c0902
Rod Morgado <rodrigo@zooft.company>
2022-01-06 21:34:18 -0800

    running the hclust algorithm with a height of 15,000 and visualizing fours clusters created

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -156,3 +156,23 @@ clust_customers <- cutree(hc_customers, h = 15000)
 
 # Generate the segmented customers data frame
 segment_customers <- mutate(customers_spend, cluster = clust_customers)
+
+dist_customers <- dist(customers_spend)
+hc_customers <- hclust(dist_customers)
+clust_customers <- cutree(hc_customers, h = 15000)
+segment_customers <- mutate(customers_spend, cluster = clust_customers)
+
+# Count the number of customers that fall into each cluster
+count(segment_customers, cluster)
+
+# Color the dendrogram based on the height cutoff
+dend_customers <- as.dendrogram(hc_customers)
+dend_colored <- color_branches(dend_customers, h = 15000)
+
+# Plot the colored dendrogram
+plot(dend_colored)
+
+# Calculate the mean for each category
+segment_customers %>% 
+  group_by(cluster) %>% 
+  summarise_all(list(mean))

71f91ee2d36240a6dc1eb00126d74c29d835d98c
Rod Morgado <rodrigo@zooft.company>
2022-01-07 14:42:11 -0800

    creating a data frame with the sum of the squares from each k (number of clusters) to plot the elbow representation

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -202,3 +202,17 @@ lineup_km3 <- mutate(lineup, cluster = clust_km3)
 # Plot the positions of the players and color them using their cluster
 ggplot(lineup_km3, aes(x = x, y = y, color = factor(cluster))) +
   geom_point()
+
+library(purrr)
+
+# Use map_dbl to run many models with varying value of k (centers)
+tot_withinss <- map_dbl(1:10,  function(k){
+  model <- kmeans(x = lineup, centers = k)
+  model$tot.withinss
+})
+
+# Generate a data frame containing both k and tot_withinss
+elbow_df <- data.frame(
+  k = 1:10 ,
+  tot_withinss = tot_withinss
+)

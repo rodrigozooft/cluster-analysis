20aeb88c1ad510b9aa41ba48a51202e8cc89452e
Rod Morgado <rodrigo@zooft.company>
2022-01-06 16:38:58 -0800

    creating cluster using hclust, method complete and mutating the tibble to add a new column

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -62,3 +62,15 @@ single
 # Calculate the average distance between group 1-2 and 3
 average <- mean(c(distance_1_3, distance_2_3))
 average
+
+# Calculate the Distance
+dist_players <- dist(lineup)
+
+# Perform the hierarchical clustering using the complete linkage
+hc_players <- hclust(dist_players, method = "complete")
+
+# Calculate the assignment vector with a k of 2
+clusters_k2 <- cutree(hc_players, k = 2)
+
+# Create a new data frame storing these results
+lineup_k2_complete <- mutate(lineup, cluster = clusters_k2)

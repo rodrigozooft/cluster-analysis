ea0dd326750d619c08eca263f7f2babe4ea19dbc
Rod Morgado <rodrigo@zooft.company>
2022-01-07 14:43:44 -0800

    plotting the "elbow" representation to find the best number of clusters

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -216,3 +216,20 @@ elbow_df <- data.frame(
   k = 1:10 ,
   tot_withinss = tot_withinss
 )
+
+# Use map_dbl to run many models with varying value of k (centers)
+tot_withinss <- map_dbl(1:10,  function(k){
+  model <- kmeans(x = lineup, centers = k)
+  model$tot.withinss
+})
+
+# Generate a data frame containing both k and tot_withinss
+elbow_df <- data.frame(
+  k = 1:10,
+  tot_withinss = tot_withinss
+)
+
+# Plot the elbow plot
+ggplot(elbow_df, aes(x = k, y = tot_withinss)) +
+  geom_line() +
+  scale_x_continuous(breaks = 1:10)

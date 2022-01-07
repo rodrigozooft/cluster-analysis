b335f91cfe92b23e154a6b58a67b0488f135840d
Rod Morgado <rodrigo@zooft.company>
2022-01-07 14:32:51 -0800

    running the same kmenas model, but now are using 3 clusters

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -189,3 +189,16 @@ lineup_km2 <- mutate(lineup, cluster = clust_km2)
 # Plot the positions of the players and color them using their cluster
 ggplot(lineup_km2, aes(x = x, y = y, color = factor(cluster))) +
   geom_point()
+
+# Build a kmeans model
+model_km3 <- kmeans(lineup, centers = 3)
+
+# Extract the cluster assignment vector from the kmeans model
+clust_km3 <- model_km3$cluster
+
+# Create a new data frame appending the cluster assignment
+lineup_km3 <- mutate(lineup, cluster = clust_km3)
+
+# Plot the positions of the players and color them using their cluster
+ggplot(lineup_km3, aes(x = x, y = y, color = factor(cluster))) +
+  geom_point()

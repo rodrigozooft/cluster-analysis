fc39d9bf10141f619485aacb2fea433f9a86ac9c
Rod Morgado <rodrigo@zooft.company>
2022-01-07 14:53:56 -0800

    performing silhouette analysis to check if the elements are well classified in their clusters

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -233,3 +233,18 @@ elbow_df <- data.frame(
 ggplot(elbow_df, aes(x = k, y = tot_withinss)) +
   geom_line() +
   scale_x_continuous(breaks = 1:10)
+
+ library(cluster)
+
+# Generate a k-means model using the pam() function with a k = 2
+pam_k2 <- pam(lineup, k = 2)
+
+# Plot the silhouette visual for the pam_k2 model
+plot(silhouette(pam_k2))
+
+# Generate a k-means model using the pam() function with a k = 3
+pam_k3 <- pam(lineup, k = 3)
+
+# Plot the silhouette visual for the pam_k3 model
+
+plot(silhouette(pam_k3)) 

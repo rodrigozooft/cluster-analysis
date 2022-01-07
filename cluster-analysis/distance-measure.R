6551ef8ed11350e1f9644d962a209f62f074afe8
Rod Morgado <rodrigo@zooft.company>
2022-01-06 21:22:26 -0800

    using as.dendogram and color_branches to show a dendogram and its representation

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -96,3 +96,25 @@ par(mfrow = c(1,3))
 plot(hc_complete, main = 'Complete Linkage')
 plot(hc_single, main = 'Single Linkage')
 plot(hc_average, main = 'Average Linkage')
+
+library(dendextend)
+dist_players <- dist(lineup, method = 'euclidean')
+hc_players <- hclust(dist_players, method = "complete")
+
+# Create a dendrogram object from the hclust variable
+dend_players <- as.dendrogram(hc_players)
+
+# Plot the dendrogram
+plot(dend_players)
+
+# Color branches by cluster formed from the cut at a height of 20 & plot
+dend_20 <- color_branches(dend_players, h = 20)
+
+# Plot the dendrogram with clusters colored below height 20
+plot(dend_20)
+
+# Color branches by cluster formed from the cut at a height of 40 & plot
+dend_40 <- color_branches(dend_players, h = 40)
+
+# Plot the dendrogram with clusters colored below height 40
+plot(dend_40)

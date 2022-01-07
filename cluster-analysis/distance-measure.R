49735e1f79ae00d34fa4e0946d9a9e38c1276ce1
Rod Morgado <rodrigo@zooft.company>
2022-01-06 21:26:13 -0800

    plotting two clustering representation with different height (20 and 40)

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -118,3 +118,26 @@ dend_40 <- color_branches(dend_players, h = 40)
 
 # Plot the dendrogram with clusters colored below height 40
 plot(dend_40)
+
+dist_players <- dist(lineup, method = 'euclidean')
+hc_players <- hclust(dist_players, method = "complete")
+
+# Calculate the assignment vector with a h of 20
+clusters_h20 <- cutree(hc_players, h = 20)
+
+# Create a new data frame storing these results
+lineup_h20_complete <- mutate(lineup, cluster = clusters_h20)
+
+# Calculate the assignment vector with a h of 40
+clusters_h40 <- cutree(hc_players, h = 40)
+
+# Create a new data frame storing these results
+lineup_h40_complete <- mutate(lineup, cluster = clusters_h40)
+
+# Plot the positions of the players and color them using their cluster for height = 20
+ggplot(lineup_h20_complete, aes(x = x, y = y, color = factor(cluster))) +
+  geom_point()
+
+# Plot the positions of the players and color them using their cluster for height = 40
+ggplot(lineup_h40_complete, aes(x = x, y = y, color = factor(cluster))) +
+  geom_point()

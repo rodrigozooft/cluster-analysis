b1972b5463a3a23710ad251347d3a99c57437413
Rod Morgado <rodrigo@zooft.company>
2022-01-07 15:00:06 -0800

    using pam function to extract the silhouette average value for each k (cluster) and choosing the one with the highest

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -247,4 +247,21 @@ pam_k3 <- pam(lineup, k = 3)
 
 # Plot the silhouette visual for the pam_k3 model
 
-plot(silhouette(pam_k3)) 
+plot(silhouette(pam_k3))
+
+# Use map_dbl to run many models with varying value of k
+sil_width <- map_dbl(2:10,  function(k){
+  model <- pam(x = customers_spend, k = k)
+  model$silinfo$avg.width
+})
+
+# Generate a data frame containing both k and sil_width
+sil_df <- data.frame(
+  k = 2:10,
+  sil_width = sil_width
+)
+
+# Plot the relationship between k and sil_width
+ggplot(sil_df, aes(x = k, y = sil_width)) +
+  geom_line() +
+  scale_x_continuous(breaks = 2:10)

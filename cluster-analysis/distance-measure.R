fc8823eefae93b7b23fe342e44b278421e4ca5db
Rod Morgado <rodrigo@zooft.company>
2022-01-07 16:37:23 -0800

    preparing the data for hierarchical clustering

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -302,3 +302,24 @@ dend_colored <- color_branches(dend_oes, h = 100000)
 
 # Plot the colored dendrogram
 plot(dend_colored)
+
+dist_oes <- dist(oes, method = 'euclidean')
+hc_oes <- hclust(dist_oes, method = 'average')
+
+library(tibble)
+library(tidyr)
+
+# Use rownames_to_column to move the rownames into a column of the data frame
+df_oes <- rownames_to_column(as.data.frame(oes), var = 'occupation')
+
+# Create a cluster assignment vector at h = 100,000
+cut_oes <- cutree(hc_oes, h = 100000)
+
+# Generate the segmented the oes data frame
+clust_oes <- mutate(df_oes, cluster = cut_oes)
+
+# Create a tidy data frame by gathering the year and values into two columns
+gathered_oes <- gather(data = clust_oes, 
+                       key = year, 
+                       value = mean_salary, 
+                       -occupation, -cluster)

dea9cf2da39aedf3e73801fc6ba6267600d459fe
Rod Morgado <rodrigo@zooft.company>
2022-01-07 16:49:58 -0800

    using Silhouette Widths the number of clusters recommended is 7

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -346,3 +346,20 @@ elbow_df <- data.frame(
 ggplot(elbow_df, aes(x = k, y = tot_withinss)) +
   geom_line() +
   scale_x_continuous(breaks = 1:10)
+
+  # Use map_dbl to run many models with varying value of k
+sil_width <- map_dbl(2:10,  function(k){
+  model <- pam(oes, k = k)
+  model$silinfo$avg.width
+})
+
+# Generate a data frame containing both k and sil_width
+sil_df <- data.frame(
+  k = 2:10,
+  sil_width = sil_width
+)
+
+# Plot the relationship between k and sil_width
+ggplot(sil_df, aes(x = k, y = sil_width)) +
+  geom_line() +
+  scale_x_continuous(breaks = 2:10)

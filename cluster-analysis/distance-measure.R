099500372e6baa1251da1f35615f44f9dce63b68
Rod Morgado <rodrigo@zooft.company>
2022-01-07 16:47:08 -0800

    using the elbow analysis to find the best k that optimize the error of clustering

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -329,3 +329,20 @@ sort(cut_oes)
 # Plot the relationship between mean_salary and year and color the lines by the assigned cluster
 ggplot(gathered_oes, aes(x = year, y = mean_salary, color = factor(cluster))) + 
     geom_line(aes(group = occupation))
+
+# Use map_dbl to run many models with varying value of k (centers)
+tot_withinss <- map_dbl(1:10,  function(k){
+  model <- kmeans(x = oes, centers = k)
+  model$tot.withinss
+})
+
+# Generate a data frame containing both k and tot_withinss
+elbow_df <- data.frame(
+  k = 1:10,
+  tot_withinss = tot_withinss
+)
+
+# Plot the elbow plot
+ggplot(elbow_df, aes(x = k, y = tot_withinss)) +
+  geom_line() +
+  scale_x_continuous(breaks = 1:10)

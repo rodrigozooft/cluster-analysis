fb9e59f852ca2cbf9f0923393795389f57b83553
Rod Morgado <rodrigo@zooft.company>
2022-01-07 16:40:33 -0800

    getting the results from clustering using hclust

--- cluster-analysis/distance-measure.R
+++ cluster-analysis/distance-measure.R
@@ -323,3 +323,9 @@ gathered_oes <- gather(data = clust_oes,
                        key = year, 
                        value = mean_salary, 
                        -occupation, -cluster)
+# View the clustering assignments by sorting the cluster assignment vector
+sort(cut_oes)
+
+# Plot the relationship between mean_salary and year and color the lines by the assigned cluster
+ggplot(gathered_oes, aes(x = year, y = mean_salary, color = factor(cluster))) + 
+    geom_line(aes(group = occupation))
